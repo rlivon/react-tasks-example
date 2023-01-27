76ec861beccbda1760c0dd568f14a3c2d1e63d45
Raul Livon <raullivon@gmail.com>
2023-01-27 10:27:15 -0300

    Primer commit

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,43 @@
+import { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskForm() {
+  const [title, setTitle] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const { createTask } = useContext(TaskContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createTask({
+      title,
+      descripcion,
+    });
+    setTitle("");
+    setDescripcion("");
+  };
+  return (
+    <div className="max-w-md mx-auto">
+      <form className="bg-zinc-800 p-10 mb-4" onSubmit={handleSubmit}>
+        <h1 className="text-2xl font-bold text-white mb-3">Crea Tu tarea</h1>
+        <input
+          className="bg-slate-300 p-3 w-full mb-2"
+          placeholder="Escribe tu tarea: "
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          autoFocus
+        />
+        <textarea
+          className="bg-slate-300 p-3 w-full mb-2"
+          placeholder="Escribe la descripción de tu tarea: "
+          onChange={(e) => setDescripcion(e.target.value)}
+          value={descripcion}
+        />
+        <button
+        className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
